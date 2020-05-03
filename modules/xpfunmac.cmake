53239cf0510ff7af8ccd186a0ac616c6a33d7d4e
Cameron Frandsen <cameron.frandsen1@gmail.com>
2020-05-03 15:07:15 -0600

    remove clang-format

--- modules/xpfunmac.cmake
+++ modules/xpfunmac.cmake
@@ -1066,23 +1066,6 @@ macro(xpSourceListAppend)
         file(REMOVE ${CMAKE_BINARY_DIR}/notincmake.txt)
       endif()
       ####
-      # Windows can't handle passing very many files to clang-format
-      if(NOT MSVC AND fmtFiles AND NOT ${CMAKE_PROJECT_NAME} STREQUAL externprolite)
-        # make paths relative to CMAKE_SOURCE_DIR
-        xpListRemoveFromAll(fmtFiles ${CMAKE_SOURCE_DIR} . ${fmtFiles})
-        list(LENGTH fmtFiles lenFmtFiles)
-        xpGetPkgVar(clangformat EXE)
-        add_custom_command(OUTPUT format_cmake
-          COMMAND ${CLANGFORMAT_EXE} -style=file -i ${fmtFiles}
-          WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-          COMMENT "Running clang-format on ${lenFmtFiles} files..."
-          )
-        string(REPLACE ";" "\n" fmtFiles "${fmtFiles}")
-        file(WRITE ${CMAKE_BINARY_DIR}/formatfiles.txt ${fmtFiles}\n)
-        add_custom_target(format SOURCES ${CMAKE_BINARY_DIR}/formatfiles.txt DEPENDS format_cmake)
-        list(APPEND masterSrcList ${CMAKE_BINARY_DIR}/formatfiles.txt)
-        set_property(TARGET format PROPERTY FOLDER CMakeTargets)
-      endif()
     endif() # is a .git repo
     option(XP_CSCOPE "always update cscope database" OFF)
     if(XP_CSCOPE)
